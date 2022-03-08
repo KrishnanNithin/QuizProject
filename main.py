4ef7c221b62d6cf7c96f9c335c9d2fe8ff1ad5cf
KrishnanNithin <nithinsk22@gmail.com>
2022-03-08 21:27:00 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for info in question_data:
+    newq = Question(info['text'], info['answer'])
+    question_bank.append(newq)
