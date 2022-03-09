d2735f0609ed8ff0bf1ffee86e8c75d56e639cf2
KrishnanNithin <nithinsk22@gmail.com>
2022-03-09 18:37:25 +0400

    Completes functionality for the quiz

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
+score = 0
+
 for info in question_data:
     newq = Question(info['text'], info['answer'])
     question_bank.append(newq)
 
+
+brain = QuizBrain(question_bank)
+while brain.still_has_questions():
+    brain.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was {brain.score}/{brain.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,6 +3,22 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = list
 
+    def still_has_questions(self):
+        return len(self.question_list) > self.question_number
+
     def next_question(self):
         qs = self.question_list[self.question_number]
-        answer = input(f'Q.{self.question_number+1}: {qs} (True or False)')
+        self.question_number += 1
+        answer = input(f'Q.{self.question_number}: {qs.text} (True or False) ')
+        self.check_answer(answer, qs.answer)
+
+    score = 0
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong..")
+            print(f"The correct answer was {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print()
